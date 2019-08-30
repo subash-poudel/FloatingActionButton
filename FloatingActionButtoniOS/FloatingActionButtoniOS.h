f74bde023227f2e24bea1d40c474b50deba3319d
Subash Poudel <blogsubash@gmail.com>
2019-08-30 18:49:11 +0545

    master Add ability to use FloatingActionButton via carthage

---
+++ FloatingActionButtoniOS/FloatingActionButtoniOS.h
@@ -0,0 +1,19 @@
+//
+//  FloatingActionButtoniOS.h
+//  FloatingActionButtoniOS
+//
+//  Created by Subash Poudel on 8/30/19.
+//  Copyright © 2019 Subash Poudel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingActionButtoniOS.
+FOUNDATION_EXPORT double FloatingActionButtoniOSVersionNumber;
+
+//! Project version string for FloatingActionButtoniOS.
+FOUNDATION_EXPORT const unsigned char FloatingActionButtoniOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingActionButtoniOS/PublicHeader.h>
+
+
